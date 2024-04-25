dbd5a85133a2bf8b9de2f365bc440ba949b1ddd9
MohamedEmadHamida <128223543+MohamedEmadHamida@users.noreply.github.com>
2024-04-25 20:40:31 +0200

    databasenew

--- DB/Database.sql
+++ DB/Database.sql
@@ -18,3 +18,15 @@ description VARCHAR(100),
 price DECIMAL(6, 2),
 quantity_gram INT
 );
+
+
+INSERT INTO FoodItems (name, description, price, quantity)
+VALUES ('Sandwich', 'Chicken Sandwich', 5.99, 10),
+       ('Salad', 'Caesar Salad', 7.49, 15),
+       ('Pizza', 'Margherita Pizza', 8.99, 8);
+
+-- Insert sample data into Drinks table
+INSERT INTO Drinks (name, description, price, quantity_gram)
+VALUES ('Coffee', 'Espresso', 2.49, 250),
+       ('Tea', 'Green Tea', 1.99, 200),
+       ('Smoothie', 'Mixed Berry Smoothie', 4.99, 300);
